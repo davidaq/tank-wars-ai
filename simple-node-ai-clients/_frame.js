68f7989686bfd397f7cc92ea281e77dfb231e7df
DavidAQ <153019747@qq.com>
2017-11-05 18:41:31 +0800

    proxy ai

--- simple-node-ai-clients/_frame.js
+++ simple-node-ai-clients/_frame.js
@@ -1,10 +1,19 @@
 const fetch = require('isomorphic-fetch');
 const co = require('co');
 
-const api = process.argv[2];
+const api = process.argv[2] || 'http://localhost:8776';
 
 module.exports = solver => co(function* () {
-  let state = yield fetch(api, { method: 'GET' }).then(r => r.json());
+  let state;
+  while (true) {
+    try {
+      state = yield fetch(api, { method: 'GET' }).then(r => r.json());
+      break;
+    } catch (err) {
+      console.log('Not ready...');
+      yield new Promise(cb => setTimeout(cb), 1000);
+    }
+  }
   let i = 0;
   while (!state.ended) {
     process.stdout.write(`  ${i++}\t my: ${state.myTank.length}\t enemy: ${state.enemyTank.length}\t\t\t\r`);
--- simple-node-ai-clients/proxy.js
+++ simple-node-ai-clients/proxy.js
@@ -2,6 +2,13 @@ const frame = require('./_frame');
 
 frame((state, obj) => {
   state.myTank.forEach(tank => {
-    obj[tank.id] = {};
+    obj[tank.id] = {
+      x: state.terain[0].length - 1,
+      y: state.terain.length - 1,
+      direction: 'up',
+      attack: 0,
+      travel: 1,
+      dodge: 10000,
+    };
   });
 });
