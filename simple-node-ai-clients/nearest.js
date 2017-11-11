27e145bee5060edb74d218d01c73e4f64507dfa8
anqin <153019747@qq.com>
2017-11-12 00:26:47 +0800

    nearest

--- simple-node-ai-clients/nearest.js
+++ simple-node-ai-clients/nearest.js
@@ -1,6 +1,14 @@
 const frame = require('./_frame');
 
+let fired;
+
 frame((state, obj) => {
+  if (!fired) {
+    fired = {};
+    state.myTank.forEach(tank => {
+      fired[tank.id] = 0;
+    });
+  }
   state.myTank.forEach(tank => {
     let dist = 100;
     let target = null;
@@ -10,7 +18,28 @@ frame((state, obj) => {
         dist = d;
         target = enemy;
       }
-    })
+    });
+    if (fired[tank.id] == 0) {
+      if (target.x == tank.x) {
+        if (Math.abs(target.y - tank.y) < 10) {
+          fired[tank.id] = 4;
+          obj[tank.id] = {
+            action: target.y < tank.y ? 'fire-up' : 'fire-down',
+          };
+          return;
+        }
+      } else if (target.y == tank.y) {
+        if (Math.abs(target.x - tank.x) < 10) {
+          fired[tank.id] = 4;
+          obj[tank.id] = {
+            action: target.x < tank.x ? 'fire-left' : 'fire-right',
+          };
+          return;
+        }
+      }
+    } else {
+      fired[tank.id]--;
+    }
     obj[tank.id] = {
       // x: Math.floor(state.terain[0].length / 2),
       // y: Math.floor(state.terain.length / 2),
