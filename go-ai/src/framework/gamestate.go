0a0481976141483b17efdefbe7be8ce7a398e8b3
linxingchen <linxingchen@baidu.com>
2017-11-13 02:22:37 +0800

    修复雷达和躲避系统到左上角问题

--- go-ai/src/framework/gamestate.go
+++ go-ai/src/framework/gamestate.go
@@ -2,7 +2,6 @@ package framework
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 type GameState struct {
@@ -173,7 +172,7 @@ func ParseGameState (bytes []byte) (*GameState, error) {
 		Ended: dat["ended"].(bool),
 	}
 	// parse terain
-	fmt.Println(dat["terain"])
+	//fmt.Println(dat["terain"])
 	for _, iline := range dat["terain"].([]interface{}) {
 		line := iline.([]interface{})
 		ret.Terain.Width = len(line)
