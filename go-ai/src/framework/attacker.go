7b6c1fad6a4e3d1cf9fed231be8277b65769983c
weiaiwen <weiaiwen@hotmail.com>
2017-11-12 16:07:38 +0800

    attack up

--- go-ai/src/framework/attacker.go
+++ go-ai/src/framework/attacker.go
@@ -5,7 +5,8 @@
 package framework
 
 import (
-	"math"
+	"math";
+	"fmt";
 )
 
 type Attacker struct {
@@ -17,135 +18,239 @@ func NewAttacker() *Attacker {
 	return inst
 }
 
-/**
- * 计算火线位置
- */
-func (self *Attacker) calcFireLine(tank *Tank, state *GameState, fireline *[]Position) {
+func calcSin (theTank Tank, tanks []Tank, fireDirection int, bulletSpeed int) {
+	for _, otherTank := range tanks {
+		if theTank.Id == otherTank.Id {
+			continue
+		}
 
-	switch tank.Pos.Direction {
-		case DirectionUp:
-			for y := tank.Pos.Y - 1; y >= 0; y-- {
-				if 1 == state.Terain.Get(tank.Pos.X, y) {
-					break
-				}
-				*fireline = append(*fireline, Position{X: tank.Pos.X, Y: y, Direction: tank.Pos.Direction})
-			}
-		case DirectionDown:
-			for y := tank.Pos.Y + 1; y < state.Terain.Height; y++ {
-				if 1 == state.Terain.Get(tank.Pos.X, y) {
-					break
-				}
-				*fireline = append(*fireline, Position{X: tank.Pos.X, Y: y, Direction: tank.Pos.Direction})
-			}
-		case DirectionLeft:
-			for x := tank.Pos.X - 1; x >= 0; x-- {
-				if 1 == state.Terain.Get(x, tank.Pos.Y) {
-					break
-				}
-				*fireline = append(*fireline, Position{X: x, Y: tank.Pos.Y, Direction: tank.Pos.Direction})
-			}
-		case DirectionRight:
-			for x := tank.Pos.X + 1; x < state.Terain.Width; x++ {
-				if 1 == state.Terain.Get(x, tank.Pos.Y) {
-					break
-				}
-				*fireline = append(*fireline, Position{X: x, Y: tank.Pos.Y, Direction: tank.Pos.Direction})
-			}
-	}
-}
+		fx := theTank.Pos.X
+		fy := theTank.Pos.Y
+		ox := otherTank.Pos.X
+		oy := otherTank.Pos.Y
 
-func (self *Attacker) firelineEmeny(tank *Tank, state *GameState, fireline *[]Position) (fire bool, urgent int){
-	// 初始化
-	fire = false
-	urgent = 0
-
-	// 如果火线上，障碍物以前有地方坦克，直接射击
-	for _, enemytank := range state.EnemyTank{
-		if (enemytank.Pos.X == tank.Pos.X || enemytank.Pos.Y == tank.Pos.Y) {
-			// 如果根本不在一条直线上，无需考虑
-			if 0 == len(*fireline) {
-				self.calcFireLine(tank, state, fireline)
+		switch fireDirection {
+		case QUADRANT_U: 
+			if ox == fx && fy > oy &&  fy - oy <= bulletSpeed {
+				return 1
 			}
-			// 如果敌方坦克正好在fireline上则发射 并计算还有多少步
-			for _, firelinev := range (*fireline) {
-				// 这个地方之后再优化
-				if (firelinev.X == enemytank.Pos.X && firelinev.Y == enemytank.Pos.Y) {
-					return true, int(math.Abs(float64(enemytank.Pos.X - tank.Pos.X)) + math.Abs(float64(enemytank.Pos.Y - tank.Pos.Y)))
-				}
+		case QUADRANT_L:
+			if oy == fy && fx > ox && fx - ox <= bulletSpeed {
+				return 1
+			}
+		case QUADRANT_D:
+			if ox == fx && oy > fy && oy - fy <= bulletSpeed {
+				return 1
+			}
+		case QUADRANT_R:
+			if oy == fy && ox > fx && ox - fx <= bulletSpeed {
+				return 1
 			}
 		}
 	}
-
-	return fire, urgent
+	return 0
 }
 
-func (self *Attacker) prefire(tank *Tank, state *GameState, fireline *[]Position) (fire bool, urgent int){
-	// 初始化
-	fire = false
-	urgent = 0
+func calcFaith (distances, bulletSpeed int) {
+	if distance <= bulletSpeed {
+		return 1
+	}
+	return 0
+}
 
-	// 如果火线两侧的位置，有朝向火线的坦克，则射击
-	for _, enemytank := range state.EnemyTank{
-		switch tank.Pos.Direction {
-		case DirectionUp:
-		case DirectionDown:
-			if (enemytank.Pos.Direction != DirectionLeft && enemytank.Pos.Direction != DirectionRight) {
-				continue
+func calcCost (tank Tank, fireDirection int, bulletSpeed int, terain Terain) {
+	cost := 1
+	switch fireDirection {
+	case QUADRANT_U:
+		for i := tank.Pos.Y - 1; i >= 0; i-- {
+			if terain.Get(tank.Pos.X, i) == 1 {
+				return math.Ceil(float64(cost) / float64(bulletSpeed))
 			}
-		case DirectionLeft:
-		case DirectionRight:
-			if (enemytank.Pos.Direction != DirectionDown && enemytank.Pos.Direction != DirectionUp) {
-				continue
-			}
+			cost += 1
 		}
+	case QUADRANT_L:
+		for i := tank.Pos.X - 1; i >= 0; i-- {
+			if terain.Get(i, tank.Pos.Y) == 1 {
+				return math.Ceil(float64(cost) / float64(bulletSpeed))
+			}
+			cost += 1
+		}
+	case QUADRANT_D:
+		for i := tank.Pos.Y + 1; i < terain.Height; i++ {
+			if terain.Get(tank.Pos.X, i) == 1 {
+				return math.Ceil(float64(cost) / float64(bulletSpeed))
+			}
+			cost += 1
+		}
+	case QUADRANT_R:
+		for i := tank.Pos.X + 1; i < terain.Width; i++ {
+			if terain.Get(i, tank.Pos.Y) == 1 {
+				return math.Ceil(float64(cost) / float64(bulletSpeed))
+			}
+			cost += 1
+		}
+	}
+}
 
-		// 预测6格即可，即6格火线
-		for i := 0; i < 6; i++ {
-			if len(*fireline) > i {
-				// 监测火线两侧敌军
-				if (*fireline)[i].X == enemytank.Pos.X || (*fireline)[i].Y == enemytank.Pos.Y {
-					// 敌军到火线的距离
-					tmpFEDis := int(math.Abs(float64(enemytank.Pos.X - (*fireline)[i].X)) + math.Abs(float64(enemytank.Pos.Y - (*fireline)[i].Y)))
-					if tmpFEDis == int(math.Ceil(float64(tmpFEDis) / 2)) {
-						return true, i
+func (self *Radar) attack(state *GameState, enemyThreats *map[string][]EnemyThreat) (map[string]RadarFireAll) {
+	radarFireAlls := make(map[string]RadarFireAll)
+
+	for _, tank := range state.MyTank {
+		for _, enemyThreat := range (*enemyThreats)[tank.Id] {
+			faith := 0
+			sin := 0
+			cost := 0
+			if len(enemyThreat.Distances) == 1 {
+				for fireDirection, dist := range enemyThreat.Distances {
+					faith = calcFaith(dist, state.Params.BulletSpeed)
+					sin = calcSin(tank, state.MyTank, fireDirection, state.Params.BulletSpeed)
+					cost = calcCost(tank, fireDirection, state.Params.BulletSpeed, state.Terain)
+					switch fireDirection {
+					case QUADRANT_U:
+						radarFireAlls[tank.Id].Up = RadarFire {Faith: faith, Cost: cost, Sin: sin, Action: ActionFireUp}
+					case QUADRANT_L:
+						radarFireAlls[tank.Id].Left = RadarFire {Faith: faith, Cost: cost, Sin: sin, Action: ActionFireLeft}
+					case QUADRANT_D:
+						radarFireAlls[tank.Id].Down = RadarFire {Faith: faith, Cost: cost, Sin: sin, Action: ActionFireDown}
+					case QUADRANT_R:
+						radarFireAlls[tank.Id].Right = RadarFire {Faith: faith, Cost: cost, Sin: sin, Action: ActionFireRight}
 					}
 				}
 			}
 		}
 	}
 
-	return fire, urgent
+	return radarFireAlls
 }
+/**
+ * 计算火线位置
+ */
+// func (self *Attacker) calcFireLine(tank *Tank, state *GameState, fireline *[]Position) {
 
-func (self *Attacker) Suggest(tank *Tank, state *GameState, objective *Objective) {
-	// 定义默认值
-	fire	:= false
-	action 	:= ActionNone
-	urgent 	:= 0
+// 	switch tank.Pos.Direction {
+// 		case DirectionUp:
+// 			for y := tank.Pos.Y - 1; y >= 0; y-- {
+// 				if 1 == state.Terain.Get(tank.Pos.X, y) {
+// 					break
+// 				}
+// 				*fireline = append(*fireline, Position{X: tank.Pos.X, Y: y, Direction: tank.Pos.Direction})
+// 			}
+// 		case DirectionDown:
+// 			for y := tank.Pos.Y + 1; y < state.Terain.Height; y++ {
+// 				if 1 == state.Terain.Get(tank.Pos.X, y) {
+// 					break
+// 				}
+// 				*fireline = append(*fireline, Position{X: tank.Pos.X, Y: y, Direction: tank.Pos.Direction})
+// 			}
+// 		case DirectionLeft:
+// 			for x := tank.Pos.X - 1; x >= 0; x-- {
+// 				if 1 == state.Terain.Get(x, tank.Pos.Y) {
+// 					break
+// 				}
+// 				*fireline = append(*fireline, Position{X: x, Y: tank.Pos.Y, Direction: tank.Pos.Direction})
+// 			}
+// 		case DirectionRight:
+// 			for x := tank.Pos.X + 1; x < state.Terain.Width; x++ {
+// 				if 1 == state.Terain.Get(x, tank.Pos.Y) {
+// 					break
+// 				}
+// 				*fireline = append(*fireline, Position{X: x, Y: tank.Pos.Y, Direction: tank.Pos.Direction})
+// 			}
+// 	}
+// }
 
-	// 火线
-	var fireline []Position
+// func (self *Attacker) firelineEmeny(tank *Tank, state *GameState, fireline *[]Position) (fire bool, urgent int){
+// 	// 初始化
+// 	fire = false
+// 	urgent = 0
 
-	// 当前火线方向是否有敌军
-	fire, fireUrgent := self.firelineEmeny(tank, state, &fireline)
-	if fire == true {
-		action = ActionFireUp
-		urgent = int(math.Ceil(float64(fireUrgent / 2)))
-	}
+// 	// 如果火线上，障碍物以前有地方坦克，直接射击
+// 	for _, enemytank := range state.EnemyTank{
+// 		if (enemytank.Pos.X == tank.Pos.X || enemytank.Pos.Y == tank.Pos.Y) {
+// 			// 如果根本不在一条直线上，无需考虑
+// 			if 0 == len(*fireline) {
+// 				self.calcFireLine(tank, state, fireline)
+// 			}
+// 			// 如果敌方坦克正好在fireline上则发射 并计算还有多少步
+// 			for _, firelinev := range (*fireline) {
+// 				// 这个地方之后再优化
+// 				if (firelinev.X == enemytank.Pos.X && firelinev.Y == enemytank.Pos.Y) {
+// 					return true, int(math.Abs(float64(enemytank.Pos.X - tank.Pos.X)) + math.Abs(float64(enemytank.Pos.Y - tank.Pos.Y)))
+// 				}
+// 			}
+// 		}
+// 	}
 
-	// 当前火线方向附近是否有敌军
-	if fire == false {
-		fire, fireUrgent := self.prefire(tank, state, &fireline)
-		if fire == true {
-			action = ActionFireUp
-			urgent = int(math.Ceil(float64(fireUrgent) / 2))
-		}
-	}
+// 	return fire, urgent
+// }
 
-	_, _ = action, urgent
-	// ret := SuggestionItem {
-	// 	Action: action,
-	// 	Urgent: urgent,
-	// }
-	// return ret
-}
+// func (self *Attacker) prefire(tank *Tank, state *GameState, fireline *[]Position) (fire bool, urgent int){
+// 	// 初始化
+// 	fire = false
+// 	urgent = 0
+
+// 	// 如果火线两侧的位置，有朝向火线的坦克，则射击
+// 	for _, enemytank := range state.EnemyTank{
+// 		switch tank.Pos.Direction {
+// 		case DirectionUp:
+// 		case DirectionDown:
+// 			if (enemytank.Pos.Direction != DirectionLeft && enemytank.Pos.Direction != DirectionRight) {
+// 				continue
+// 			}
+// 		case DirectionLeft:
+// 		case DirectionRight:
+// 			if (enemytank.Pos.Direction != DirectionDown && enemytank.Pos.Direction != DirectionUp) {
+// 				continue
+// 			}
+// 		}
+
+// 		// 预测6格即可，即6格火线
+// 		for i := 0; i < 6; i++ {
+// 			if len(*fireline) > i {
+// 				// 监测火线两侧敌军
+// 				if (*fireline)[i].X == enemytank.Pos.X || (*fireline)[i].Y == enemytank.Pos.Y {
+// 					// 敌军到火线的距离
+// 					tmpFEDis := int(math.Abs(float64(enemytank.Pos.X - (*fireline)[i].X)) + math.Abs(float64(enemytank.Pos.Y - (*fireline)[i].Y)))
+// 					if tmpFEDis == int(math.Ceil(float64(tmpFEDis) / 2)) {
+// 						return true, i
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return fire, urgent
+// }
+
+// func (self *Attacker) Suggest(tank *Tank, state *GameState, objective *Objective) {
+// 	// 定义默认值
+// 	fire	:= false
+// 	action 	:= ActionNone
+// 	urgent 	:= 0
+
+// 	// 火线
+// 	var fireline []Position
+
+// 	// 当前火线方向是否有敌军
+// 	fire, fireUrgent := self.firelineEmeny(tank, state, &fireline)
+// 	if fire == true {
+// 		action = ActionFireUp
+// 		urgent = int(math.Ceil(float64(fireUrgent / 2)))
+// 	}
+
+// 	// 当前火线方向附近是否有敌军
+// 	if fire == false {
+// 		fire, fireUrgent := self.prefire(tank, state, &fireline)
+// 		if fire == true {
+// 			action = ActionFireUp
+// 			urgent = int(math.Ceil(float64(fireUrgent) / 2))
+// 		}
+// 	}
+
+// 	_, _ = action, urgent
+// 	// ret := SuggestionItem {
+// 	// 	Action: action,
+// 	// 	Urgent: urgent,
+// 	// }
+// 	// return ret
+// }
