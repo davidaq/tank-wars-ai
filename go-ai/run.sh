fa270f3c0399cda433282b9df92170295be1a056
魏艾文 <weiaiwen@iwaimai.baidu.com>
2017-11-16 15:14:46 +0800

    up

--- go-ai/run.sh
+++ go-ai/run.sh
@@ -2,12 +2,13 @@ cd `dirname $0`
 export GOPATH=`pwd`
 
 export HOST=ml.niven.cn:8777
-export GAME=ByCiO1qJM
+export GAME=B1ngSs9yM
 export SIDE=red
 export TACTICS=nearest
 # export TACTICS=proxy PROXY_PORT=8776
 
-go run src/ai-client.go
+go run src/ai-client.go &
+SIDE=blue TACTICS=killall go run src/ai-client.go
 
 # run forever
 # yes|while read x; do go run src/ai-client.go; done
