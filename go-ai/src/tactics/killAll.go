98d372044a098345620767a6c094d9f49b7ffc82
weiaiwen <weiaiwen@hotmail.com>
2017-11-19 15:23:51 +0800

    killAll up

--- go-ai/src/tactics/killAll.go
+++ go-ai/src/tactics/killAll.go
@@ -16,19 +16,10 @@ func (self *KillAll) Init(state *f.GameState) {
 }
 
 func (self *KillAll) Plan(state *f.GameState, radar *f.RadarResult, objective map[string]f.Objective) {
+	count := 0
 	tankloop: for _, tank := range state.MyTank {
-		least := 99999
-		var ttank *f.Tank
-		for _, etank := range state.EnemyTank {
-			dist := abs(tank.Pos.X - etank.Pos.X) + abs(tank.Pos.Y - etank.Pos.Y)
-			if dist < least {
-				ttank = &etank
-				least = dist
-			}
-		}
-
 		fmt.Println("----------------------------", tank.Id, radar.Dodge[tank.Id].Threat)
-		if radar.Dodge[tank.Id].Threat >= 0.2 && radar.Dodge[tank.Id].Threat < 1 {
+		if radar.Dodge[tank.Id].Threat >= 0.2 && radar.Dodge[tank.Id].Threat < 1 && (tank.Pos.X != 13 && tank.Pos.Y != 6 + count) {
 			ifDodge := true
 			for _, oTank := range state.MyTank {
 				if oTank.Id == tank.Id {
@@ -57,12 +48,50 @@ func (self *KillAll) Plan(state *f.GameState, radar *f.RadarResult, objective ma
 			}
 		}
 
+		// least := 99999
+		// var ttank *f.Tank
+		// for _, etank := range state.EnemyTank {
+		// 	dist := abs(tank.Pos.X - etank.Pos.X) + abs(tank.Pos.Y - etank.Pos.Y)
+		// 	if dist < least {
+		// 		ttank = &etank
+		// 		least = dist
+		// 	}
+		// }
 
-		if ttank != nil {
+		// if ttank != nil {
+		// 	objective[tank.Id] = f.Objective {
+		// 		Action: f.ActionTravel,
+		// 		Target: ttank.Pos,
+		// 	}
+		// }
+
+		if tank.Pos.X == 13 && tank.Pos.Y == 6 + count {
+			objective[tank.Id] = f.Objective {
+				Action: f.ActionFireLeft,
+			}
+		} else {
 			objective[tank.Id] = f.Objective {
 				Action: f.ActionTravel,
-				Target: ttank.Pos,
+				Target: f.Position { X: 13, Y:6 + count, Direction: f.DirectionLeft },
 			}
+		}
+		count += 1
+	}
+	bottomTank := state.MyTank[0]
+	maxY := 0	
+	count = 0
+	for _, tank := range state.MyTank {		
+		if tank.Pos.X == 13 && tank.Pos.Y == 6 + count {
+			if tank.Pos.Y > maxY {
+				maxY = tank.Pos.Y
+				bottomTank = tank
+			}
+		}
+		count += 1
+	}
+	if bottomTank.Pos.X == 13 && bottomTank.Pos.Y == maxY {		
+		objective[bottomTank.Id] = f.Objective {
+			Action: f.ActionFireDown,
 		}
 	}
 }
