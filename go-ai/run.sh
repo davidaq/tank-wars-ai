604573b50769d98c1074f88c50c684241051a681
jelewine <jelewine@gmail.com>
2017-11-26 00:47:50 +0800

    fox

--- go-ai/run.sh
+++ go-ai/run.sh
@@ -4,7 +4,7 @@ export GOPATH=`pwd`
 export HOST=ml.niven.cn:8777
 export GAME=Sy5L7-DlG
 export SIDE=blue
-export TACTICS=nearest
+export TACTICS=cattycat
 # export TACTICS=proxy PROXY_PORT=8776
 
 go run src/ai-client.go > /dev/null 2>&1 &
