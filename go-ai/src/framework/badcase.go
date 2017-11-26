1b9d7b72b036a07a6046ce1b08c2b9fc332ffae2
DavidAQ <153019747@qq.com>
2017-11-26 23:05:30 +0800

    bad case

---
+++ go-ai/src/framework/badcase.go
@@ -0,0 +1,8 @@
+// 写死防止干蠢事
+
+func BadCase(state *f.GameState, radar *f.RadarResult, movements map[string]int) {
+}
+
+// 矫正在危险位置的坦克行为
+func fixMove (state *f.GameState, radar *f.RadarResult, movements map[string]int, tank Tank) int {
+}
