138838f106bc28589259d9bd3bb31ef74d17c8f9
DavidAQ <153019747@qq.com>
2017-11-27 00:13:29 +0800

    badcase

--- go-ai/src/framework/badcase.go
+++ go-ai/src/framework/badcase.go
@@ -4,6 +4,63 @@ package framework
 
 func BadCase(state *GameState, radar *RadarResult, movements map[string]int) {
 	badCaseDangerZone(state, radar, movements)
+	badCaseShootSelf(state, radar, movements)
+}
+
+func badCaseShootSelf(state *GameState, radar *RadarResult, movements map[string]int) {
+	noPass := make(map[Position]bool)
+	noStop := make(map[Position]bool)
+	ePos := make(map[Position]bool)
+	for _, tank  := range state.MyTank {
+		noPass[tank.Pos.NoDirection()] = true
+		action := movements[tank.Id]
+		if action == ActionMove {
+			pos := tank.Pos.NoDirection()
+			for i := 0; i < state.Params.TankSpeed; i++ {
+				nPos := pos.step(tank.Pos.Direction)
+				if state.Terain.Get(nPos.X, nPos.Y) == 1 {
+					break
+				}
+				pos = nPos
+			}
+			noStop[pos] = true
+		}
+	}
+	for _, etank  := range state.EnemyTank {
+		ePos[etank.Pos] = true
+	}
+	tankloop: for _, tank  := range state.MyTank {
+		action := movements[tank.Id]
+		if action >= ActionFireUp && action <= ActionFireRight {
+			dir := action - ActionFireUp + DirectionUp
+			pos := tank.Pos.NoDirection().step(dir)
+			if ePos[pos] {
+				continue tankloop
+			}
+			if noPass[pos] || noStop[pos] {
+				movements[tank.Id] = ActionStay
+				continue tankloop
+			}
+			for i := 0; i < state.Params.BulletSpeed; i++ {
+				pos = pos.step(dir)
+				if noPass[pos] {
+					movements[tank.Id] = ActionStay
+					continue tankloop
+				}
+			}
+			if noStop[pos] {
+				movements[tank.Id] = ActionStay
+				continue tankloop
+			}
+			for i := 0; i < state.Params.BulletSpeed; i++ {
+				pos = pos.step(dir)
+				if noPass[pos] {
+					movements[tank.Id] = ActionStay
+					continue tankloop
+				}
+			}
+		}
+	}
 }
 
 func badCaseDangerZone(state *GameState, radar *RadarResult, movements map[string]int) {
@@ -40,7 +97,9 @@ func badCaseDangerZone(state *GameState, radar *RadarResult, movements map[strin
 					preferDirection[dir] = true
 				}
 			}
-			fixMove(state, radar, movements, tank, preferDirection)
+			if len(preferDirection) > 0 {
+				fixMove(state, radar, movements, tank, preferDirection)
+			}
 		}
 	}
 }
