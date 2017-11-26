22579c841882aa6daca6c08a7ff3ff5a7272fa51
DavidAQ <153019747@qq.com>
2017-11-26 23:32:30 +0800

    badcase

--- go-ai/src/framework/badcase.go
+++ go-ai/src/framework/badcase.go
@@ -3,6 +3,8 @@
 func BadCase(state *f.GameState, radar *f.RadarResult, movements map[string]int) {
 	dangerous := make(map[Position]bool)
 	directions := []int { DirectionUp, DirectionLeft, DirectionDown, DirectionRight }
+	vDirections := []int { DirectionUp, DirectionDown }
+	hDirections := []int { DirectionLeft, DirectionRight }
 	for _, eTank := range state.EnemyTank {
 		for _, dir := range directions {
 			preferVertical := true
@@ -21,11 +23,15 @@ func BadCase(state *f.GameState, radar *f.RadarResult, movements map[string]int)
 	}
 	for _, tank := range state.MyTank {
 		if prefer, danger := dangerous[tank.Pos.NoDirection()]; danger {
-			
+			preferAction := make(map[int]bool)
+			dirs := hDirections
+			if preferVertical {
+
+			}
 		}
 	}
 }
 
 // 矫正在危险位置的坦克行为
-func fixMove (state *f.GameState, radar *f.RadarResult, movements map[string]int, tank Tank, preferAction map[int]bool) {
+func fixMove (state *f.GameState, radar *f.RadarResult, movements map[string]int, tank Tank, preferDirection map[int]bool) {
 }
